1c764c5facb1cb08661f5627742997a318b66d03
Taka005 <01taka005@gmail.com>
2023-06-07 09:38:24 +0900

    修正

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-test
+<?php
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    $res["success"] = true;
+    $res["message"] = null;
+    $res["data"] = "TEST";
+    
+    print json_encode($res,JSON_UNESCAPED_SLASHES|JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_UNESCAPED_UNICODE);
+?>
