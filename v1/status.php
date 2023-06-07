9284e05c9140d10e454156e84dd9b857afbcebed
Taka005 <01taka005@gmail.com>
2023-06-07 13:58:20 +0900

    修正

--- v1/status.php
+++ v1/status.php
@@ -3,9 +3,12 @@
 
     header("Content-Type: application/json; charset=UTF-8");
     
+    $data = db("SELECT * FROM log ORDER BY time ASC;");
+
+
     $res["success"] = true;
     $res["message"] = null;
-    $res["data"] = "Hello";
+    $res["data"] = $data;
     
     print json_encode($res,JSON_UNESCAPED_SLASHES|JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_UNESCAPED_UNICODE);
 ?>
