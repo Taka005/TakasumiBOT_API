294176ee671364576406998286b27840c213c6fe
Taka005 <takanosumika.sub@gmail.com>
2023-07-31 20:12:17 +0900

    fix

--- lib.php
+++ lib.php
@@ -13,4 +13,29 @@
             return ".png";
         }
     }
+
+    function Get($url){
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url); 
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $res = json_decode(curl_exec($ch));
+        curl_close($ch);
+        
+        return $res;
+    }
+
+    function Post($url,$body){
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url); 
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_POST,true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($body));
+        curl_setopt($ch,CURLOPT_HTTPHEADER,array(
+            "Content-type: application/json"
+        ));
+        $res = json_decode(curl_exec($ch),true);
+        curl_close($ch);
+        
+        return $res;
+    }
 ?>
---
+++ v1/discord/guilds.php
@@ -0,0 +1,32 @@
+<?php
+    $config = require_once __DIR__."/../../config.php";
+    require_once __DIR__."/../../lib.php";
+    
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    
+    $guilds = Post("https://api.gakerbot.net/guilds",array(
+        "token"=>$config["token"]
+    ));
+
+    if($res["success"]){
+
+        $res["success"] = true;
+        $res["message"] = null;
+        $res["data"] = [];
+
+        foreach ($guilds as $guild) {
+            $res["data"][$guild["id"]] = [
+                "name" => $guild["name"],
+                "memberCount" => $guild["approximate_member_count"],
+                "presenceCount" => $guild["approximate_presence_count"]
+            ];
+        }
+    }else{
+        $res["success"] = false;
+        $res["message"] = "Unknown";
+        $res["data"] = null;
+    }
+    
+    print json_encode($res,JSON_UNESCAPED_SLASHES|JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_UNESCAPED_UNICODE);
+?>
