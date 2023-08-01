ca8cf6e4b0bb440748ca09d8da4564d4427b0502
Taka005 <01taka005@gmail.com>
2023-08-02 00:09:38 +0900

    エンコード調整

--- lib.php
+++ lib.php
@@ -1,7 +1,7 @@
 <?php
     function db($query){
         $config = require_once __DIR__."/config.php";
-        $pdo = new PDO("mysql:host=".$config["db_host"].";dbname=".$config["db_name"].";charset=utf8",$config["db_user"],$config["db_password"]);
+        $pdo = new PDO("mysql:host=".$config["db_host"].";dbname=".$config["db_name"].";charset=utf8mb4",$config["db_user"],$config["db_password"]);
         return $pdo->query($query);
     }
 
