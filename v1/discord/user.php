f99bd920767507b8787c78beffa2f655f3363ac1
Taka005 <01taka005@gmail.com>
2024-01-14 10:42:45 +0900

    fix

--- v1/discord/user.php
+++ v1/discord/user.php
@@ -1,15 +1,15 @@
 <?php
     $config = require_once __DIR__."/../../config.php";
     require_once __DIR__."/../../lib.php";
-    
+
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
-    
+
     if(isset($_GET["id"])){
         $id = htmlspecialchars($_GET["id"]);
 
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL,"https://discord.com/api/v10/users/".$id); 
+        curl_setopt($ch,CURLOPT_URL,"https://discord.com/api/v10/users/".$id);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch,CURLOPT_HTTPHEADER,array(
             "Authorization: Bot ".$config["token"],
@@ -20,7 +20,7 @@
 
         if(!isset($user["message"])){
             $avatar = !is_null($user["avatar"])?"https://cdn.discordapp.com/avatars/".$user["id"]."/".$user["avatar"].is_animated($user["avatar"])."?size=1024" : null;
-            $banner = !is_null($user["avatar"])?"https://cdn.discordapp.com/banners/".$user["id"]."/".$user["banner"].is_animated($user["banner"])."?size=1024" : null;
+            $banner = !is_null($user["banner"])?"https://cdn.discordapp.com/banners/".$user["id"]."/".$user["banner"].is_animated($user["banner"])."?size=1024" : null;
 
             $res["success"] = true;
             $res["message"] = null;
@@ -49,6 +49,6 @@
         $res["message"] = "UserID Not Found";
         $res["data"] = null;
     }
-    
+
     print json_encode($res,JSON_UNESCAPED_SLASHES|JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_UNESCAPED_UNICODE);
 ?>
